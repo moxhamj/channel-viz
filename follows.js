b46db3a433cf49716bc81939272034abb6d62726
moxhamj <moxhamj@internode.on.net>
2015-09-06 08:44:11 +0930

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'shsCNFtxuGELLZx8ehqglXAgDo9lkyBam5Zj22p3g3urH2FM', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [970253233], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'James Moxham', // Replaces Xively logo in the header
-		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
